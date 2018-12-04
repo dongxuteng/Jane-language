408af84d81d9306533bc453188e15c7c32557055
cuiyiming1998 <1269567664@qq.com>
2018-12-04 11:16:46 +0800

    signUp 2nd

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import $ from 'jquery';
 
@@ -9,18 +9,30 @@ import $ from 'jquery';
 })
 export class SignupPage {
 
+  @ViewChild('#username') username;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
   }
 
+  // 点击注册触发
   goLogin(){
+    console.log(this.username);
     $.ajax({
       type: 'post',
       url: 'http://localhost:8100/#/signup',
       data: {
-        
+        phonenumber: $('#phonenumber').val(),
+        phonepwd: $('#phonepwd').val(),
+        username: $('#username').val(),
+        password: $('#password').val()
+      },
+      success: function(data){
+        if(data.status == 1){
+          alert('注册成功！');
+        }
       }
     })
     this.navCtrl.pop();
