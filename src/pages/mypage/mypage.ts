cb71e4510149481cd7055aa74782cf68bedb1356
cuiyiming1998 <1269567664@qq.com>
2018-12-05 19:11:18 +0800

    myPage

--- src/pages/mypage/mypage.ts
+++ src/pages/mypage/mypage.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MypagePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-mypage',
@@ -17,14 +10,16 @@ export class MypagePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-arr=[{
-  name:'花瓣',
-  funs:2650,
-  focus:10,
-  msg:'玩手机没有用，早，努力学习吧！',
-  img:'',
-  tx:'../../assets/imgs/icon.png'
-}]
+
+  person={
+    username:'花颜',
+    funs:2650,
+    focus:10,
+    msg:'玩手机没有用，早，努力学习吧！',
+    img:'',
+    iconSrc:'../../assets/imgs/test.png'
+  }
+
   ionViewDidLoad() {
     let elements = document.querySelectorAll(".tabbar");
     if (elements != null) {
