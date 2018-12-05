1a0b67f21e0e47b610a168a1d9b4a1e82fd53cda
wusanrong915 <2085067543@qq.com>
2018-12-05 08:59:59 +0800

    welcome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { Storage } from '@ionic/storage';
+import { WelcomePage } from '../pages/welcome/welcome';
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
+  template: `<ion-nav [root]="rootPage"></ion-nav>`,
 })
 export class MyApp {
-  rootPage:any = 'LoginPage';
+  rootPage:any = WelcomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, public storage: Storage,splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
