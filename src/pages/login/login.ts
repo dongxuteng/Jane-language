f8bcfe1845bf160569f3265ab99142799152ccf4
cuiyiming1998 <1269567664@qq.com>
2018-12-18 10:08:24 +0800

    setRoot

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,16 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, App, AlertController } from 'ionic-angular';
-import { TabsPage } from '../tabs/tabs';
+import {
+  Component
+} from '@angular/core';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  App,
+  AlertController
+} from 'ionic-angular';
+import {
+  TabsPage
+} from '../tabs/tabs';
 import $ from 'jquery'
 
 
@@ -14,72 +24,70 @@ export class LoginPage {
   username: string;
   pwd: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public app: App, public alertCtrl: AlertController) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public app: App, public alertCtrl: AlertController) {}
 
   ionViewDidLoad() {
     let elements = document.querySelectorAll(".tabbar");
     if (elements != null) {
-       Object.keys(elements).map((key) => {
-          elements[key].style.display = 'none';
-     });
-    }  
+      Object.keys(elements).map((key) => {
+        elements[key].style.display = 'none';
+      });
+    }
   }
 
-  goSignup(){
+  goSignup() {
     this.navCtrl.push('SignupPage');
   }
 
-  
+
   // 登录验证,返回首页
-  login(){
+  login() {
     console.log(this.username);
     console.log(this.pwd);
-    if(this.pwd.length > 6 && this.username.length > 6){ 
+    if (this.pwd.length > 6 && this.username.length > 6) {
       $.ajax({
         type: 'post',
         url: 'http://localhost:8100/#/login',
         data: {
-          username:this.username,
-          password:this.pwd
+          username: this.username,
+          password: this.pwd
         },
-        success: function(data) {
-          if(data.id == 6){
+        success: function (data) {
+          if (data.id == 6) {
             alert(data.status);
-          }
-          else if(data.id == 1){
+          } else if (data.id == 1) {
             console.log(data.status);
             alert(data.me);
           }
         }
       })
-      this.app.getRootNav().setRoot(TabsPage);
     }
     // else if(this.username == undefined || this.pwd == undefined){
-    //   const alert = this.alertCtrl.create({
-    //     title: '错误',
-    //     subTitle: '请输入用户名或密码',
-    //     buttons: ['好']
-    //   });
-    //   alert.present();
-    // }
-    else{
-      const alert = this.alertCtrl.create({
-        title: '错误',
-        subTitle: '请输入正确的用户名和密码',
-        buttons: ['好']
-      });
-      alert.present();
-    }
-  } 
+      //   const alert = this.alertCtrl.create({
+        //     title: '错误',
+        //     subTitle: '请输入用户名或密码',
+        //     buttons: ['好']
+        //   });
+        //   alert.present();
+        // }
+        else {
+          const alert = this.alertCtrl.create({
+            title: '错误',
+            subTitle: '请输入正确的用户名和密码',
+            buttons: ['好']
+          });
+          alert.present();
+        }
+        this.app.getRootNav().setRoot(TabsPage);
+  }
 
   //ionic当退出页面的时候触发的方法
   ionViewWillLeave() {
     let elements = document.querySelectorAll(".tabbar");
     if (elements != null) {
-	   Object.keys(elements).map((key) => {
-    		elements[key].style.display = 'flex';
-	    });
+      Object.keys(elements).map((key) => {
+        elements[key].style.display = 'flex';
+      });
     }
   }
 
