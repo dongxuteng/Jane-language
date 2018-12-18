eedc261fe6455f79bb870d90a6170f27eef3595e
cuiyiming1998 <1269567664@qq.com>
2018-12-18 10:36:04 +0800

    format

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,59 +1,56 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import $ from 'jquery';
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import $ from "jquery";
 
 @IonicPage()
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html',
+  selector: "page-signup",
+  templateUrl: "signup.html"
 })
 export class SignupPage {
+  @ViewChild("#username") username;
 
-  @ViewChild('#username') username;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
+    console.log("ionViewDidLoad SignupPage");
     let elements = document.querySelectorAll(".tabbar");
     if (elements != null) {
-       Object.keys(elements).map((key) => {
-          elements[key].style.display = 'none';
+      Object.keys(elements).map(key => {
+        elements[key].style.display = "none";
       });
-    } 
+    }
   }
 
   // 点击注册触发
-  goLogin(){
+  goLogin() {
     console.log(this.username);
     $.ajax({
-      type: 'post',
-      url: 'http://localhost:8100/#/signup',
+      type: "post",
+      url: "http://localhost:8100/#/signup",
       data: {
-        phonenumber: $('#phonenumber').val(),
-        phonepwd: $('#phonepwd').val(),
-        username: $('#username').val(),
-        password: $('#password').val()
+        phonenumber: $("#phonenumber").val(),
+        phonepwd: $("#phonepwd").val(),
+        username: $("#username").val(),
+        password: $("#password").val()
       },
-      success: function(data){
-        if(data.status == 1){
-          alert('注册成功！');
+      success: function(data) {
+        if (data.status == 1) {
+          alert("注册成功！");
         }
       }
-    })
+    });
     this.navCtrl.pop();
   }
 
-  ionViewDidEnter(){  
-  }
+  ionViewDidEnter() {}
   //ionic当退出页面的时候触发的方法
   ionViewWillLeave() {
     let elements = document.querySelectorAll(".tabbar");
     if (elements != null) {
-	   Object.keys(elements).map((key) => {
-    		elements[key].style.display = 'flex';
-	    });
+      Object.keys(elements).map(key => {
+        elements[key].style.display = "flex";
+      });
     }
   }
 }
