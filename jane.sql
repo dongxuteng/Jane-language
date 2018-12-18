ad1223bbe0d7ac6dc4f5b81498f30d39df5ae8c5
董旭腾 <260739028@qq.com>
2018-12-18 17:09:50 +0800

    Add files via upload

--- jane.sql
+++ jane.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.14  Distrib 5.5.60-MariaDB, for Linux (x86_64)
 --
--- Host: localhost    Database: Jane
+-- Host: localhost    Database: jane
 -- ------------------------------------------------------
 -- Server version	5.5.60-MariaDB
 
@@ -23,11 +23,11 @@ DROP TABLE IF EXISTS `admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `UserName` varchar(24) COLLATE utf8_unicode_ci NOT NULL,
-  `UserPass` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `UserPass` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `admin` (
 
 LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` VALUES (2,'a','0cc175b9c0'),(4,'admin','e10abc3949');
+INSERT INTO `admin` VALUES (1,'a','0cc175b9c0f1b6a831c399e269772661'),(2,'admin','e10adc3949ba59abbe56e057f20f883e'),(3,'d','8277e0910d750195b448797616e091ad');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,18 +48,20 @@ DROP TABLE IF EXISTS `article`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `article` (
-  `id` int(11) NOT NULL,
-  `sort` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `author` varchar(24) COLLATE utf8_unicode_ci NOT NULL,
-  `date` datetime NOT NULL,
-  `content` text COLLATE utf8_unicode_ci NOT NULL,
-  `Uid` int(11) NOT NULL,
-  `username` varchar(24) COLLATE utf8_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sort` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `author` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `content` text COLLATE utf8_unicode_ci,
+  `Uid` int(11) DEFAULT NULL,
+  `star1` int(12) DEFAULT NULL,
+  `comments` int(12) DEFAULT NULL,
+  `transmits` int(12) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `article_ibfk_1` (`Uid`),
   CONSTRAINT `article_ibfk_1` FOREIGN KEY (`Uid`) REFERENCES `user` (`Uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +70,7 @@ CREATE TABLE `article` (
 
 LOCK TABLES `article` WRITE;
 /*!40000 ALTER TABLE `article` DISABLE KEYS */;
-INSERT INTO `article` VALUES (1,'美食','一些能吃掉黑眼圈的最佳食物','林夕','2018-12-03 15:41:09','这里推荐四种食物，可以让你省却因购买昂贵奢侈的眼霜、眼膜而“大出血”，仅需在日常饮食中增加其摄入量，就能使你和黑眼圈轻松说拜拜了。鸡蛋、芝麻、胡萝卜、海带',2,'lx'),(2,'情感','减肥，那是光进来的地方','花颜','2018-08-22 15:42:42','来说说关于爱、成长和意义。如果说，减肥需要一个终极理由的话，我认为，那就只有爱了。心理学上说，一个人之所以会浪费时间，挥霍健康，是因为他没有感受到足够被爱，于是觉得自己的生命毫无价值。我们也许都曾经想尽办法争取过别人的关注，也许不遗余力地付出过，最终，无奈的我们，只能选择自我放逐，来逃避那些不被爱的痛苦。假如，全世界真的没有人爱你了。你自己会不会做那个，唯一还爱自己的人？我们只能做一个饥肠辘辘的人，只想着向别人讨要食物，却拿不出食物，喂饱自己和帮助别人吗？自爱，然后爱人，方能被爱，难道不是这样吗？而另一种可能的情况是，我们把随心所欲当成了自爱和自由。好几年前，有一种长的四四方方的西瓜，我听说是为了运输方便，所以弄个铁盒子给套住，等西瓜长大了，就会长成方形的，也不知道是不是真的。我不知道，我们会不会也是通过这样方式成长起来的。《今日简史》中说，现在的人类，已经如同曾经被自己驯化过的家畜，我们就像人工培育的奶牛，性格温顺，乳量惊人，但在其他方面远远不及其野生祖先，没那么灵活，没那么好奇，也没那么懂得变通。今天的我们，饱食终日，却又焦虑不安，欲壑难填。我们以为跟随自己的欲望，就是自爱跟自由，殊不知，这些欲望，甚至有可能是被驯化出来的，因为世界需要这样的我们。认真的减一次肥，放下对食物和欲望的执着，或者你才能感受到自己真正想要的是什么。',1,'hy'),(3,'旅行','这个世界很大，愿你们都去看看','李梅','2018-05-23 09:43:26','从06年第一次出国开始，整整过去了11年的时间。期间不知不觉走过了全世界6大洲55个国家。住过青旅十人间，也住过五星级酒店。吃过美味路边摊，也吃过米其林。体验过开飞机，也徒步过百内W环线。跟家人、朋友、恋人旅行过，也享受过很多次独自一人的旅行。看到过乌尤尼盐湖的天空之境，也参观过乌干达坎帕拉的贫民窟。有过狂喜，也有过挫折，但唯一不变的是对这个世界的热爱和欣喜。',5,'lm');
+INSERT INTO `article` VALUES (1,'美食','一些能吃掉黑眼圈的最佳食物','林夕','2018-12-03 15:41:09','这里推荐四种食物，可以让你省却因购买昂贵奢侈的眼霜、眼膜而“大出血”，仅需在日常饮食中增加其摄入量，就能使你和黑眼圈轻松说拜拜了。鸡蛋、芝麻、胡萝卜、海带。',2,12,20,14),(2,'情感','减肥，那是光进来的地方','花颜','2018-12-17 15:42:42','来说说关于爱、成长和意义。如果说，减肥需要一个终极理由的话，我认为，那就只有爱了。心理学上说，一个人之所以会浪费时间，挥霍健康，是因为他没有感受到足够被爱，于是觉得自己的生命毫无价值。我们也许都曾经想尽办法争取过别人的关注，也许不遗余力地付出过，最终，无奈的我们，只能选择自我放逐，来逃避那些不被爱的痛苦。假如，全世界真的没有人爱你了。你自己会不会做那个，唯一还爱自己的人？我们只能做一个饥肠辘辘的人，只想着向别人讨要食物，却拿不出食物，喂饱自己和帮助别人吗？自爱，然后爱人，方能被爱，难道不是这样吗？而另一种可能的情况是，我们把随心所欲当成了自爱和自由。好几年前，有一种长的四四方方的西瓜，我听说是为了运输方便，所以弄个铁盒子给套住，等西瓜长大了，就会长成方形的，也不知道是不是真的。我不知道，我们会不会也是通过这样方式成长起来的。《今日简史》中说，现在的人类，已经如同曾经被自己驯化过的家畜，我们就像人工培育的奶牛，性格温顺，乳量惊人，但在其他方面远远不及其野生祖先，没那么灵活，没那么好奇，也没那么懂得变通。今天的我们，饱食终日，却又焦虑不安，欲壑难填。我们以为跟随自己的欲望，就是自爱跟自由，殊不知，这些欲望，甚至有可能是被驯化出来的，因为世界需要这样的我们。认真的减一次肥，放下对食物和欲望的执着，或者你才能感受到自己真正想要的是什么。',1,3,8,65),(3,'旅行','这个世界很大，愿你们都去看看','李梅','2018-11-23 09:43:26','从06年第一次出国开始，整整过去了11年的时间。期间不知不觉走过了全世界6大洲55个国家。住过青旅十人间，也住过五星级酒店。吃过美味路边摊，也吃过米其林。体验过开飞机，也徒步过百内W环线。跟家人、朋友、恋人旅行过，也享受过很多次独自一人的旅行。看到过乌尤尼盐湖的天空之境，也参观过乌干达坎帕拉的贫民窟。有过狂喜，也有过挫折，但唯一不变的是对这个世界的热爱。',5,34,53,21);
 /*!40000 ALTER TABLE `article` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +98,7 @@ CREATE TABLE `back` (
 
 LOCK TABLES `back` WRITE;
 /*!40000 ALTER TABLE `back` DISABLE KEYS */;
-INSERT INTO `back` VALUES (1,'hy','很喜欢','2018-12-05 00:00:00','花颜'),(2,'lx','支持','2018-12-12 00:00:00','林夕'),(3,'ly','推广','2018-12-11 00:00:00','林一');
+INSERT INTO `back` VALUES (1,'hy','很喜欢','2018-12-05 00:00:00','花颜'),(2,'lx','支持','2018-12-12 00:00:00','林夕'),(3,'xiongda','界面还需要继续优化','2018-12-17 14:19:01','熊大'),(4,'xionger','功能还不太完善','2018-12-17 14:43:39','熊二');
 /*!40000 ALTER TABLE `back` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -186,6 +188,29 @@ INSERT INTO `follower` VALUES (1,'hy',2,'花颜'),(2,'lx',3,'林夕'),(3,'ly',1,
 UNLOCK TABLES;
 
 --
+-- Table structure for table `img_admin`
+--
+
+DROP TABLE IF EXISTS `img_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `img_admin` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `img` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `img_admin`
+--
+
+LOCK TABLES `img_admin` WRITE;
+/*!40000 ALTER TABLE `img_admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `img_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `keyWord`
 --
 
@@ -225,8 +250,8 @@ CREATE TABLE `target` (
   `targetRelation` char(5) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`Uid`),
   KEY `target_ibfk_2` (`tid`),
-  CONSTRAINT `target_ibfk_2` FOREIGN KEY (`tid`) REFERENCES `user` (`Uid`),
-  CONSTRAINT `target_ibfk_1` FOREIGN KEY (`Uid`) REFERENCES `user` (`Uid`)
+  CONSTRAINT `target_ibfk_1` FOREIGN KEY (`Uid`) REFERENCES `user` (`Uid`),
+  CONSTRAINT `target_ibfk_2` FOREIGN KEY (`tid`) REFERENCES `user` (`Uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -260,6 +285,7 @@ CREATE TABLE `trends` (
   `name` varchar(24) COLLATE utf8_unicode_ci DEFAULT NULL,
   `audioHttp` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
   `vedioHttp` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `transmit` int(12) DEFAULT NULL,
   PRIMARY KEY (`trendsId`),
   KEY `Uid` (`Uid`),
   CONSTRAINT `trends_ibfk_1` FOREIGN KEY (`Uid`) REFERENCES `user` (`Uid`),
@@ -273,7 +299,7 @@ CREATE TABLE `trends` (
 
 LOCK TABLES `trends` WRITE;
 /*!40000 ALTER TABLE `trends` DISABLE KEYS */;
-INSERT INTO `trends` VALUES (1,'hy',1,'My Sunshine','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=84#did83','减肥，那是光进来的地方',10,'来说说关于爱、成长和意义。如果说，减肥需要一个终极理由的话，我认为，那就只有爱了。心理学上说，一个人之所以会浪费时间，挥霍健康，是因为他没有感受到足够被爱，于是觉得自己的生命毫无价值。我们也许都曾经想尽办法争取过别人的关注，也许不遗余力地付出过，最终，无奈的我们，只能选择自我放逐，来逃避那些不被爱的痛苦。假如，全世界真的没有人爱你了。你自己会不会做那个，唯一还爱自己的人？我们只能做一个饥肠辘辘的人，只想着向别人讨要食物，却拿不出食物，喂饱自己和帮助别人吗？自爱，然后爱人，方能被爱，难道不是这样吗？而另一种可能的情况是，我们把随心所欲当成了自爱和自由。好几年前，有一种长的四四方方的西瓜，我听说是为了运输方便，所以弄个铁盒子给套住，等西瓜长大了，就会长成方形的，也不知道是不是真的。我不知道，我们会不会也是通过这样方式成长起来的。《今日简史》中说，现在的人类，已经如同曾经被自己驯化过的家畜，我们就像人工培育的奶牛，性格温顺，乳量惊人，但在其他方面远远不及其野生祖先，没那么灵活，没那么好奇，也没那么懂得变通。今天的我们，饱食终日，却又焦虑不安，欲壑难填。我们以为跟随自己的欲望，就是自爱跟自由，殊不知，这些欲望，甚至有可能是被驯化出来的，因为世界需要这样的我们。认真的减一次肥，放下对食物和欲望的执着，或者你才能感受到自己真正想要的是什么。',NULL,'花颜','http://www.yue365.com/play/993/342293.shtml',NULL),(2,'lx',2,'Cry On My Shoulder','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=591#did590','一些能吃掉黑眼圈的最佳食物',50,'这里推荐四种食物，可以让你省却因购买昂贵奢侈的眼霜、眼膜而“大出血”，仅需在日常饮食中增加其摄入量，就能使你和黑眼圈轻松说拜拜了。鸡蛋、芝麻、胡萝卜、海带',NULL,'林夕','http://bd.kuwo.cn/yue365/CryOnMyShoulder/DeutschlandSuchtDenSuperstar',NULL),(3,'ly',3,'Faded','','上班族如何在30分钟内做出丰富的便当早餐',60,'1.一起床就烧热水，烧水期间把食材洗干净备用。\r\n2.水烧好后用先将米饭放入电饭煲里煮，大概20分钟可以完成。（用热水煮饭会比较快）\r\n3.煮饭空隙可以炒菜或者做费时间的菜，这样饭做好后，菜也就好了。\r\n4.烤鸡腿、鸡米花这类的提前一个晚上做，第二天复烤即可。',NULL,'林一','http://www.yue365.com/play/25467/398883.shtml',NULL),(4,'zy',4,'','','不评论就是最大的善意',80,'我本不关注娱乐圈的事，特别是报道明星的家长里短，谁谁谁出轨了，谁又和谁在网上打口水战了，我其实更多是不听、不看、不关注。众口铄金，网友过于关注，只会让事情节外生枝。慕容素衣写的这本《淡定是最好的优雅》，是一本随笔，书里写的是作者对一些女明星的看法。赵雅芝、沈殿霞、林青霞、刘嘉玲、翁美玲，等等，可以说是专门写给女人看的一本鸡汤随笔。作者的部分言论我不太赞同，但是书的封面和各种水彩植物插图真的很美、很素、很淡雅。','肖申克的救赎','左一',NULL,'https://www.jianshu.com/p/f454bf0c2c3b'),(5,'lm',5,'','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=11#did10','这个世界很大，愿你们都去看看',123,'从06年第一次出国开始，整整过去了11年的时间。期间不知不觉走过了全世界6大洲55个国家。住过青旅十人间，也住过五星级酒店。吃过美味路边摊，也吃过米其林。体验过开飞机，也徒步过百内W环线。跟家人、朋友、恋人旅行过，也享受过很多次独自一人的旅行。看到过乌尤尼盐湖的天空之境，也参观过乌干达坎帕拉的贫民窟。有过狂喜，也有过挫折，但唯一不变的是对这个世界的热爱和欣喜。','毒液','李梅',NULL,'https://www.jianshu.com/p/adbf423d62b2');
+INSERT INTO `trends` VALUES (1,'hy',1,'My Sunshine','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=84#did83','减肥，那是光进来的地方',10,'来说说关于爱、成长和意义。如果说，减肥需要一个终极理由的话，我认为，那就只有爱了。心理学上说，一个人之所以会浪费时间，挥霍健康，是因为他没有感受到足够被爱，于是觉得自己的生命毫无价值。我们也许都曾经想尽办法争取过别人的关注，也许不遗余力地付出过，最终，无奈的我们，只能选择自我放逐，来逃避那些不被爱的痛苦。假如，全世界真的没有人爱你了。你自己会不会做那个，唯一还爱自己的人？我们只能做一个饥肠辘辘的人，只想着向别人讨要食物，却拿不出食物，喂饱自己和帮助别人吗？自爱，然后爱人，方能被爱，难道不是这样吗？而另一种可能的情况是，我们把随心所欲当成了自爱和自由。好几年前，有一种长的四四方方的西瓜，我听说是为了运输方便，所以弄个铁盒子给套住，等西瓜长大了，就会长成方形的，也不知道是不是真的。我不知道，我们会不会也是通过这样方式成长起来的。《今日简史》中说，现在的人类，已经如同曾经被自己驯化过的家畜，我们就像人工培育的奶牛，性格温顺，乳量惊人，但在其他方面远远不及其野生祖先，没那么灵活，没那么好奇，也没那么懂得变通。今天的我们，饱食终日，却又焦虑不安，欲壑难填。我们以为跟随自己的欲望，就是自爱跟自由，殊不知，这些欲望，甚至有可能是被驯化出来的，因为世界需要这样的我们。认真的减一次肥，放下对食物和欲望的执着，或者你才能感受到自己真正想要的是什么。',NULL,'花颜','http://www.yue365.com/play/993/342293.shtml',NULL,NULL),(2,'lx',2,'Cry On My Shoulder','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=591#did590','一些能吃掉黑眼圈的最佳食物',50,'这里推荐四种食物，可以让你省却因购买昂贵奢侈的眼霜、眼膜而“大出血”，仅需在日常饮食中增加其摄入量，就能使你和黑眼圈轻松说拜拜了。鸡蛋、芝麻、胡萝卜、海带',NULL,'林夕','http://bd.kuwo.cn/yue365/CryOnMyShoulder/DeutschlandSuchtDenSuperstar',NULL,NULL),(3,'ly',3,'Faded','','上班族如何在30分钟内做出丰富的便当早餐',60,'1.一起床就烧热水，烧水期间把食材洗干净备用。\r\n2.水烧好后用先将米饭放入电饭煲里煮，大概20分钟可以完成。（用热水煮饭会比较快）\r\n3.煮饭空隙可以炒菜或者做费时间的菜，这样饭做好后，菜也就好了。\r\n4.烤鸡腿、鸡米花这类的提前一个晚上做，第二天复烤即可。',NULL,'林一','http://www.yue365.com/play/25467/398883.shtml',NULL,NULL),(4,'zy',4,'','','不评论就是最大的善意',80,'我本不关注娱乐圈的事，特别是报道明星的家长里短，谁谁谁出轨了，谁又和谁在网上打口水战了，我其实更多是不听、不看、不关注。众口铄金，网友过于关注，只会让事情节外生枝。慕容素衣写的这本《淡定是最好的优雅》，是一本随笔，书里写的是作者对一些女明星的看法。赵雅芝、沈殿霞、林青霞、刘嘉玲、翁美玲，等等，可以说是专门写给女人看的一本鸡汤随笔。作者的部分言论我不太赞同，但是书的封面和各种水彩植物插图真的很美、很素、很淡雅。','肖申克的救赎','左一',NULL,'https://www.jianshu.com/p/f454bf0c2c3b',NULL),(5,'lm',5,'','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=11#did10','这个世界很大，愿你们都去看看',123,'从06年第一次出国开始，整整过去了11年的时间。期间不知不觉走过了全世界6大洲55个国家。住过青旅十人间，也住过五星级酒店。吃过美味路边摊，也吃过米其林。体验过开飞机，也徒步过百内W环线。跟家人、朋友、恋人旅行过，也享受过很多次独自一人的旅行。看到过乌尤尼盐湖的天空之境，也参观过乌干达坎帕拉的贫民窟。有过狂喜，也有过挫折，但唯一不变的是对这个世界的热爱和欣喜。','毒液','李梅',NULL,'https://www.jianshu.com/p/adbf423d62b2',NULL);
 /*!40000 ALTER TABLE `trends` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -347,14 +373,14 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `Uid` int(11) NOT NULL,
+  `Uid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(24) COLLATE utf8_unicode_ci NOT NULL,
   `age` int(11) NOT NULL,
   `constellation` varchar(13) COLLATE utf8_unicode_ci DEFAULT NULL,
   `target` int(11) NOT NULL,
   `password` varchar(24) COLLATE utf8_unicode_ci NOT NULL,
   `gender` char(2) COLLATE utf8_unicode_ci NOT NULL,
-  `avatar` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `img` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
   `area` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `phoneNumber` char(11) COLLATE utf8_unicode_ci DEFAULT NULL,
   `myReply` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -366,9 +392,10 @@ CREATE TABLE `user` (
   `weChat` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `web` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `regtime` datetime DEFAULT NULL,
-  `name` varchar(24) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`Uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`Uid`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -377,7 +404,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'hy',18,'水瓶座',10,'1234567','女','http://pic.sogou.com/d?query=%CE%A2%D0%C5%D3%C3%BB%A7%CD%B7%CF%F1+%CD%BC%C6%AC&st=255&mode=255&did=15#did14','石家庄','15336525986','很喜欢你',2,5,'减肥，那时光进来的地方','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=84#did83','花颜','花颜','花颜','2018-03-08 14:48:44','花颜'),(2,'lx',30,'射手座',30,'1314258','男','http://pic.sogou.com/d?query=%CE%A2%D0%C5%D3%C3%BB%A7%CD%B7%CF%F1+%CD%BC%C6%AC&st=255&mode=255&did=152#did151','北京','15669852364','支持你',14,8000,'一些能能吃掉黑眼圈的最佳食物','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=84#did104','林夕','林夕','林夕','2017-10-12 14:48:58','林夕'),(3,'ly',20,'白羊座',20,'1658947','女','http://pic.sogou.com/d?query=%CE%A2%D0%C5%D3%C3%BB%A7%CD%B7%CF%F1+%CD%BC%C6%AC&st=255&mode=255&did=151#did150','上海','15698241539','一起去玩',3,4,'上班族如何在30分钟内做出丰富的便当早餐','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=11#did10','林一','林一','林一','2018-10-03 19:45:10','林一'),(4,'zy',22,'双子座',60,'a1b2c3d','男','http://pic.sogou.com/d?query=%E5%8D%A1%E9%80%9A+%E5%A4%B4%E5%83%8F&page=1&did=3&st=191&phu=http%3A%2F%2Fimg03.store.sogou.com%2Fapp%2Fa%2F10010016%2F20b1fadb415babf4726aa34fcd2c7cdc#did19','厦门','15528964532','吃饭',5,8,'不评论就是最大的善意','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=45#did44','左一','左一','左一','2018-10-11 08:49:22','左一'),(5,'lm',36,'金牛座',50,'ebscfd','女','http://pic.sogou.com/d?query=%E5%8D%A1%E9%80%9A+%E5%A4%B4%E5%83%8F&page=1&did=3&st=191&phu=http%3A%2F%2Fimg03.store.sogou.com%2Fapp%2Fa%2F10010016%2F20b1fadb415babf4726aa34fcd2c7cdc#did2','成都','16952384765','旅游',20,6,'这个世界很大，愿你们都去看看','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=591#did590','李梅','李梅','李梅','2016-12-15 11:49:35','李梅');
+INSERT INTO `user` VALUES (1,'hy',18,'水瓶座',10,'1234567','女','/imguser/1.jpg','石家庄','15336525986','很喜欢你',2,5,'减肥，那时光进来的地方','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=84#did83','花颜','花颜','花颜','2018-03-08 14:48:44','花颜'),(2,'lx',30,'射手座',30,'1314258','男','/imguser/2.jpg','北京','15669852364','支持你',14,8000,'一些能能吃掉黑眼圈的最佳食物','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=84#did104','林夕','林夕','林夕','2017-10-12 14:48:58','林夕'),(3,'ly',20,'白羊座',20,'1658947','女','/imguser/3.jpg','上海','15698241539','一起去玩',3,4,'上班族如何在30分钟内做出丰富的便当早餐','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=11#did10','林一','林一','林一','2018-10-03 19:45:10','林一'),(4,'zy',22,'双子座',60,'a1b2c3d','男','/imguser/4.jpg','厦门','15528964532','吃饭',5,8,'不评论就是最大的善意','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=45#did44','左一','左一','左一','2018-10-11 08:49:22','左一'),(5,'lm',36,'金牛座',50,'ebscfd','女','/imguser/5.jpg','成都','16952384765','旅游',20,6,'这个世界很大，愿你们都去看看','http://pic.sogou.com/d?query=%B1%B3%BE%B0%CD%BC%C6%AC%CE%DE%CB%AE%D3%A1&mode=1&did=591#did590','李梅','李梅','李梅','2016-12-15 11:49:35','李梅'),(6,'mx',20,'处女座',40,'dxt123456','男','/imguser/6.jpg','石家庄','15226591929','挺不错的',5,10,'今天又是美好的一天',NULL,'熊大','熊大','熊大','2018-12-18 09:32:26','熊大');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -390,4 +417,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-12 16:46:19
+-- Dump completed on 2018-12-18 17:08:07
