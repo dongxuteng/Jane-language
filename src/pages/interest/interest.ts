6745a29382bbbaa4923b244b17ceaa2e312905bf
zhangtianhu <1071406152@qq.com>
2018-12-23 16:08:02 +0800

    草稿箱

--- src/pages/interest/interest.ts
+++ src/pages/interest/interest.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the InterestPage page.
@@ -17,54 +17,13 @@ export class InterestPage {
   testCheckboxOpen: boolean;
   testCheckboxResult;
   arr;
-  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
   ionViewDidLoad() {
-    console.log('ionViewDidLoad InterestPage');
+    // console.log('ionViewDidLoad InterestPage');
   }
-  showCheckbox() {
-    let alert = this.alertCtrl.create();
-    alert.setTitle('选择你的兴趣');
-
-    alert.addInput({
-      type: 'checkbox',
-      label: '美食',
-      value: '美食',
-      checked: true
-    });
-
-    alert.addInput({
-      type: 'checkbox',
-      label: '随笔',
-      value: '随笔'
-    });
-    alert.addInput({
-      type: 'checkbox',
-      label: '情感',
-      value: '情感'
-    });
-    alert.addInput({
-      type: 'checkbox',
-      label: '旅行',
-      value: '旅行'
-    });
-
-    alert.addButton('Cancel');
-    alert.addButton({
-      text: 'Okay',
-      handler: data => {
-
-        // console.log('Checkbox data:', data);
-        this.testCheckboxOpen = false;
-        this.testCheckboxResult = data;
-        this.arr=data;
-        // for(var i=0;i<this.arr.length;i++){
-        //   console.log(this.arr[i]);
-        // }
-
-      }
-    });
-    alert.present();
+  Gofabu(){
+    this.navCtrl.push("FabuPage")
   }
 
 }
