c010212a9addbfbaa41bca642d1767d20fc2e821
zhangtianhu <1071406152@qq.com>
2018-12-25 14:47:59 +0800

    隐藏底部tab栏

--- src/pages/encouragement/encouragement.ts
+++ src/pages/encouragement/encouragement.ts
@@ -42,9 +42,23 @@ export class EncouragementPage {
   ]
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-  // ionViewDidLoad() {
-  //   console.log('ionViewDidLoad EncouragementPage');
-  // }
+  ionViewDidLoad() {
+    let elements = document.querySelectorAll(".tabbar");
+    if (elements != null) {
+      Object.keys(elements).map((key) => {
+        elements[key].style.display = 'none';
+      });
+    }
+  }
+  //ionic当退出页面的时候触发的方法
+  ionViewWillLeave() {
+    let elements = document.querySelectorAll(".tabbar");
+    if (elements != null) {
+      Object.keys(elements).map((key) => {
+        elements[key].style.display = 'flex';
+      });
+    }
+  }
   goEncourage(){
     this.navCtrl.push('EncouragemorePage')
   }
