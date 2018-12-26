0877d88cce093a48e933724251349c5c0aa8f757
wusanrong915 <2085067543@qq.com>
2018-12-26 17:22:37 +0800

    修改

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { Storage } from '@ionic/storage';
 import { LoginPage } from "../pages/login/login";
 import { TabsPage } from "../pages/tabs/tabs";
+import { HomePage } from '../pages/home/home';
+import { SightPage } from '../pages/sight/sight';
+import { WelcomePage } from '../pages/welcome/welcome';
 
 @Component({
   templateUrl: 'app.html',
@@ -14,12 +17,11 @@ export class MyApp {
   rootPage:any;
 
   constructor(platform: Platform, statusBar: StatusBar, public storage: Storage,splashScreen: SplashScreen) {
-    if(window.localStorage.getItem('username')) {
-      this.rootPage = TabsPage;
-    } else {
-      this.rootPage = LoginPage;
-    }
-    
+     if(window.localStorage.getItem('username')) {
+       this.rootPage = TabsPage;
+     } else {
+       this.rootPage = WelcomePage;
+     }
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
