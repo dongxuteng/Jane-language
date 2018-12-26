3f33edefde6520d0cc6e70a89befba868c6058f8
zhangtianhu <1071406152@qq.com>
2018-12-26 15:02:50 +0800

    imgge

--- src/pages/image/image.ts
+++ src/pages/image/image.ts
@@ -18,55 +18,84 @@ export class ImagePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-  articles = [
+  arr = [
     {
-      title: '制作传统的法国羊角面包',
-      imgSrc: '../../assets/imgs/3.jpg',
-      iconSrc: '../../assets/imgs/icon.png',
-      username: 'Kika Kong',
-      likes: '222 Likes'
+      img: '../../assets/imgs/10.jpg',
+      tx: '../../assets/imgs/icon.png',
+      name: 'Kika Kong',
     },
     {
-      title: '草莓味甜点',
-      imgSrc: '../../assets/imgs/2.jpg',
-      iconSrc: '../../assets/imgs/icon.png',
-      username: 'Kika Kong',
-      likes: '334 Likes'
+      img: '../../assets/imgs/11.jpg',
+      tx: '../../assets/imgs/icon.png',
+      name: 'Kika Kong',
     },
     {
-      title: '好看又好吃的马卡龙',
-      imgSrc: '../../assets/imgs/6.jpg',
-      iconSrc: '../../assets/imgs/icon.png',
-      username: 'Kika Kong',
-      likes: '877 Likes'
+      img: '../../assets/imgs/13.jpg',
+      tx: '../../assets/imgs/icon.png',
+      name: 'Kika Kong',
+    },
+    {
+      img: '../../assets/imgs/12.jpg',
+      tx: '../../assets/imgs/icon.png',
+      name: 'Kika Kong',
     }
   ]
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     let elements = document.querySelectorAll(".tabbar");
     if (elements != null) {
-       Object.keys(elements).map((key) => {
-          elements[key].style.display = 'none';
+      Object.keys(elements).map((key) => {
+        elements[key].style.display = 'none';
       });
     }
+  }
+  ionViewDidEnter() {
   }
   //ionic当退出页面的时候触发的方法
   ionViewWillLeave() {
     let elements = document.querySelectorAll(".tabbar");
     if (elements != null) {
-	   Object.keys(elements).map((key) => {
-    		elements[key].style.display = 'flex';
-	    });
+      Object.keys(elements).map((key) => {
+        elements[key].style.display = 'flex';
+      });
     }
   }
 
+  ionViewWillEnter() {
+    this.getNode();
+  }
+  getNode() {
+    let parentNode = document.getElementById("container");
+    let childNodeArray: any = parentNode.getElementsByClassName("box");
+    let screenWidth = document.documentElement.clientWidth;
+    let childWidth = childNodeArray[0].offsetWidth;
+    let num = Math.floor(screenWidth / childWidth); //获得一排摆的个数 用Math.floor()转换为整数
+    parentNode.style.cssText = "width:" + childWidth * num + "px; margin:0 auto"; //固定container的宽并设置居中
+    this.setImagePosition(num, childNodeArray);
+  }
 
-  doRefresh(refresher) {
-    console.log('Begin async operation', refresher);
-    setTimeout(() => {
-      console.log('Async operation has ended');
-      refresher.complete();
-    }, 2000);
+  setImagePosition(num, childArray) {
+    var imgHeightArray = [];//定义数组用于存放所有图片的高度
+    for (var i = 0; i < childArray.length; i++) { //遍历所有图片
+      if (i < num) {
+        imgHeightArray[i] = childArray[i].offsetHeight; //取得第一排图片的高度
+      } else {
+        var minHeight = Math.min.apply(null, imgHeightArray); //获取第一排图片中高度最小的图片
+        var minIndex = this.getMinHeight(imgHeightArray, minHeight); //函数获得高度最小的图片的位置
+        childArray[i].style.position = "absolute"; //绝对定位图片
+        childArray[i].style.top = minHeight + "px"; //图片距顶部像素
+        childArray[i].style.left = childArray[minIndex].offsetLeft + "px"; //图片距左的像素
+        imgHeightArray[minIndex] = imgHeightArray[minIndex] + childArray[i].offsetHeight; //将最低高度的box的高度加上它下方的box高度
+      }
+    }
+  }
+
+  getMinHeight(imgHeightArray, minHeight) {
+    for (var i in imgHeightArray) {
+      if (imgHeightArray[i] == minHeight) { //循环所有数组的高度 让它等于最小图片的高度 返回i值
+        return i;
+      }
+    }
   }
   Goperson(){
     this.navCtrl.push('PersonalPage');
