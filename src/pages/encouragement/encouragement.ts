04b727bfc962c0626d0fb1cad5eb726262d6f20c
董旭腾 <260739028@qq.com>
2019-01-01 17:24:03 +0800

    1

--- src/pages/encouragement/encouragement.ts
+++ src/pages/encouragement/encouragement.ts
@@ -40,14 +40,7 @@ export class EncouragementPage {
        //inner: '在我们走出校门的那一刻，我们就面临这选择，是选择北上广深一线大城市，还是回到自己名不见经传的老家;是选择进入制度规范、福利有保障的大公司，还是进入担任多面手的小公司;是经商，还是从政。每一种选择对于不同的人来说，无所谓好坏之分，只有是否合适之说。',
      }
    ]
-   article4=[
-    {
-      src:"../../assets/imgs/lizhi1.jpg",
-      name:'花颜',
-      title: '与其碌碌无为，不如兴风作浪',
-      imgSrc: '../../assets/imgs/11.jpg',
-      //inner:'“世间谤我、欺我、辱我、笑我、轻我、贱我、恶我、骗我、如何处治乎?只是忍他、让他、由他、避他、耐他、敬他、不要理他、再待几年你且看他。”愿你成全这个世界给予你的欢喜，也成全当年兴风作浪的自己。...',
-    }]
+   
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
   // ionViewDidLoad() {
