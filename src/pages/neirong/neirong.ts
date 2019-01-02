e7086dfb5c5d7256e84ae0909d4eb1f48f39af5c
zhangtianhu <1071406152@qq.com>
2019-01-02 14:31:27 +0800

    评论

--- src/pages/neirong/neirong.ts
+++ src/pages/neirong/neirong.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { HttpClient } from "@angular/common/http";
-import { getUnpackedSettings } from "http2";
 
 @IonicPage()
 @Component({
